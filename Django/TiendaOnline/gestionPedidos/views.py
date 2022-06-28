d232150c013bd6549b26bb02b279d34897f5f8bc
davidroses02 <davidrosasalcaraz@gmail.com>
2022-06-28 22:47:57 +0200

    Cambios

--- Django/TiendaOnline/gestionPedidos/views.py
+++ Django/TiendaOnline/gestionPedidos/views.py
@@ -1,3 +1,27 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from gestionPedidos.models import Clientes, Articulos, Pedidos
 
 # Create your views here.
+
+def busqueda_productos(request):
+    return render(request, 'busqueda_productos.html')
+
+def buscar(request):
+    if request.GET['busqueda']:
+        #mensaje = 'Articulo buscado: ' + request.GET['busqueda']
+        producto = request.GET['busqueda']
+        articulos = Articulos.objects.filter(nombre__icontains=producto)
+        return render(request, 'resultados_busqueda.html', {'articulos': articulos, "producto": producto})
+    else:
+        mensaje = 'No se ha introducido ningun articulo'
+
+    return HttpResponse(mensaje)
+
+def contacto(request):
+
+    if request.method == 'POST':
+        return render(request, 'gracias.html')
+
+    return render(request, 'contacto.html')
