a701060dabe99dcab511a6302a048e729f4e70cc
davidroses02 <davidrosasalcaraz@gmail.com>
2022-06-28 22:52:12 +0200

    seguridad

--- Django/TiendaOnline/TiendaOnline/settings.py
+++ Django/TiendaOnline/TiendaOnline/settings.py
@@ -137,4 +137,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMIAL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "davidrosasalcaraz@gmail.com"
-EMAIL_HOST_PASSWORD = "JpyreSGM74Q59Y5P"
+EMAIL_HOST_PASSWORD = ""
